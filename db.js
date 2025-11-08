073c31c0a1a46c0fbf81c6e06176e6aacff60e26
Praneet Om <om.praneet.in@gmail.com>
2025-11-08 20:28:22 +0530

    Update database connection settings in db.js
    Added port configuration to database connection.

--- db.js
+++ db.js
@@ -2,11 +2,13 @@
 import mysql from "mysql2/promise";
 
 const db = await mysql.createPool({
-  host: "mysql.railway.internal:3306",
+  host: "mysql.railway.internal",
   user: "root",
   password: "skPnJlPFlzhhMycuFTacqFokokbrXcLt", 
   database: "rideshare",
+  port: "3306",
 });
 
 // mysql://root:skPnJlPFlzhhMycuFTacqFokokbrXcLt@mysql.railway.internal:3306/rideshare
 export default db;
+
